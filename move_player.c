2bdb0756706d46708f0c94bd8473c9737ad3c6f8
ainzsoup <gamrauis@gmail.com>
2022-03-09 09:20:58 +0100

    norming...

--- move_player.c
+++ move_player.c
@@ -6,34 +6,44 @@
 /*   By: sgamraou <sgamraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 13:21:21 by sgamraou          #+#    #+#             */
-/*   Updated: 2022/03/02 13:44:21 by sgamraou         ###   ########.fr       */
+/*   Updated: 2022/03/09 09:19:01 by sgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+typedef struct t_files{
+	void	*sussy;
+	void	*map_bg;
+	void	*wall;
+}	t_files;
+
 
 int move(int keycode, t_data *data)
 {
-	static int count = 0;
+	t_files		files;
+	static int	count;
+	t_x_y		res;
+	t_files		f;
+	t_coord		cord;
+	int			c; 
 	if (keycode == 53)
 			{
 				mlx_destroy_window(data->mlx, data->win);
 				exit(0);
 			}
-	t_x_y res;
-	void *sussy = mlx_xpm_file_to_image(data->mlx, "P.xpm", &res.xp, &res.yp);
-	void *map_bg = mlx_xpm_file_to_image(data->mlx, "0.xpm", &res.x0, &res.y0);
-	void *wall = mlx_xpm_file_to_image(data->mlx, "W.xpm", &res.x1, &res.y1);
-	t_coord cord = get_coord(data);
-	int c = get_items(*data);
+	f.sussy = mlx_xpm_file_to_image(data->mlx, "P.xpm", &res.xp, &res.yp);
+	f.map_bg = mlx_xpm_file_to_image(data->mlx, "0.xpm", &res.x0, &res.y0);
+	f.wall = mlx_xpm_file_to_image(data->mlx, "W.xpm", &res.x1, &res.y1);
+	cord = get_coord(data);
+	c = get_items(*data);
 	if (keycode == 13)
 	{
 		if (data->map[cord.y - 1][cord.x] == '0' || data->map[cord.y -1][cord.x] == 'C' || (data->map[cord.y - 1][cord.x] == 'E' && c == 0))
 		{	
 			if (data->map[cord.y][cord.x] == 'C')
 				c--;
-			mlx_put_image_to_window(data->mlx, data->win, sussy, 75 * cord.x, 75 * (cord.y - 1));
-			mlx_put_image_to_window(data->mlx, data->win, map_bg, 75 * cord.x, 75 * cord.y); 
+			mlx_put_image_to_window(data->mlx, data->win, f.sussy, 75 * cord.x, 75 * (cord.y - 1));
+			mlx_put_image_to_window(data->mlx, data->win, f.map_bg, 75 * cord.x, 75 * cord.y); 
 			if (data->map[cord.y - 1][cord.x] == 'E')
 			{
 				data->map[cord.y][cord.x] = '0';
@@ -55,8 +65,8 @@ int move(int keycode, t_data *data)
 		{
 			if (data->map[cord.y][cord.x] == 'C')
 				c--;
-			mlx_put_image_to_window(data->mlx, data->win, sussy, 75 * cord.x, 75 * (cord.y + 1));
-			mlx_put_image_to_window(data->mlx, data->win, map_bg, 75 * cord.x, 75 * cord.y);
+			mlx_put_image_to_window(data->mlx, data->win, f.sussy, 75 * cord.x, 75 * (cord.y + 1));
+			mlx_put_image_to_window(data->mlx, data->win, f.map_bg, 75 * cord.x, 75 * cord.y);
 			if (data->map[cord.y + 1][cord.x] == 'E')
 			{
 				data->map[cord.y][cord.x] = '0';
@@ -78,8 +88,8 @@ int move(int keycode, t_data *data)
 		{
 			if (data->map[cord.y][cord.x] == 'C')
 				c--;
-			mlx_put_image_to_window(data->mlx, data->win, sussy, 75 * (cord.x - 1), 75 * cord.y);
-			mlx_put_image_to_window(data->mlx, data->win, map_bg, 75 * cord.x, 75 * cord.y);
+			mlx_put_image_to_window(data->mlx, data->win, f.sussy, 75 * (cord.x - 1), 75 * cord.y);
+			mlx_put_image_to_window(data->mlx, data->win, f.map_bg, 75 * cord.x, 75 * cord.y);
 			if (data->map[cord.y][cord.x - 1] == 'E')
 			{
 				data->map[cord.y][cord.x] = '0';
@@ -101,8 +111,8 @@ int move(int keycode, t_data *data)
 		{
 			if (data->map[cord.y][cord.x] == 'C')
 				c--;
-			mlx_put_image_to_window(data->mlx, data->win, sussy, 75 * (cord.x + 1), 75 * cord.y);
-			mlx_put_image_to_window(data->mlx, data->win, map_bg, 75 * cord.x, 75 * cord.y);
+			mlx_put_image_to_window(data->mlx, data->win, f.sussy, 75 * (cord.x + 1), 75 * cord.y);
+			mlx_put_image_to_window(data->mlx, data->win, f.map_bg, 75 * cord.x, 75 * cord.y);
 			if (data->map[cord.y][cord.x + 1] == 'E')
 			{
 				data->map[cord.y][cord.x] = '0';
